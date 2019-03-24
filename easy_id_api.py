e00471ce56704f9ca851734695e08503d3c15fb8
Steve Goldman <steve@stevegoldman.net>
2019-03-24 19:00:29 -0400

    Changed index to simple, ugly form

--- easy_id_api.py
+++ easy_id_api.py
@@ -8,12 +8,17 @@ Created on Sat Mar 23 14:57:04 2019
 
 import easy_id as ei
 
-from flask import Flask,json,render_template
+from flask import Flask,json,request,render_template
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/' ,methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method=="POST":
+        ist=request.form['input_string']
+        easy=ei.easy_id(ist)
+        return render_template('index.html',value=ist,gid=easy)
+    else:
+        return render_template('index.html')    
 
 @app.route('/easyid/')
 @app.route('/easyid/<some_value>')
